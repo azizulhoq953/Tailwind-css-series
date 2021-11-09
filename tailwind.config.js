dc297299ab330fc9a205e2149f680b7af337883f
azizulhoq953 <azizulhoq2041@gmail.com>
2021-11-09 11:27:59 +0600

    otherstateDark

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   purge: [],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: "media", // or 'media' or 'class'
   theme: {
     extend: {},
   },
@@ -9,4 +9,4 @@ module.exports = {
     extend: {},
   },
   plugins: [],
-}
+};
